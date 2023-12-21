8a1affb9f9b0ffaa1cca5b25ba3f3e59757315c1
Manuel Gómez Martín <gomez.maman24@triana.salesianos.edu>
2023-12-21 16:06:47 +0100

    Primer Commit

---
+++ ExamUD2TipoAManuelGomez/doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"ExamUD2TipoAManuelGomez"}];updateSearchResults();
---
+++ ExamUD2TipoAManuelGomez/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"ExamUD2TipoAManuelGomez","l":"ejercicio"}];updateSearchResults();
